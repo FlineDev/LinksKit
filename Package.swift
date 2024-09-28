a737d2ba2433c8b151a78277a024f4cd5e0b21c8
Cihat Gündüz <commits@cihatguenduez.de>
2024-09-28 07:59:00 +0200

    Fix localization configuration + localize to all iOS languages

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
    name: "LinksKit",
+   defaultLocalization: "en",
    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .visionOS(.v1), .watchOS(.v9)],
    products: [.library(name: "LinksKit", targets: ["LinksKit"])],
    targets: [.target(name: "LinksKit")]
